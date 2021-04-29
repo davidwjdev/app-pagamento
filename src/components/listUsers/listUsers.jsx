a5edc75d1c4b7255dc8c64a646ec65984fa67170
David Wilson de Jesus <David.Jesus@wedotechnologies.com>
2021-04-29 11:09:30 -0300

    adicionado folha de estilo

--- src/components/listUsers/listUsers.jsx
+++ src/components/listUsers/listUsers.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
-
+import './../../components/listUsers/listUsers.css'
 export default class ListUsers extends Component {
 
     state = {
@@ -19,41 +19,38 @@ export default class ListUsers extends Component {
     };
     render() {
         const { users } = this.state;
-
         return users.map(user => (
-            <div className="body-background"  key={user.id}>
+            <div className="body-background" key={user.id}>
                 {/* <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script> */}
-                <div className="body">
-                    <div className="user">
+                <div className="user">
+                    <div className="user-itens">
                         <div className="user-photo">
                             {/* {user.img == null ?
                                 <img src='' alt={user.name} />
                                 : <i class='far fa-user-circle'></i>} */}
                             <img src={user.img} alt={user.name} />
-
                         </div>
                         <div className="user-data">
                             <div>
-                                <label>Nome do Usuario: </label>
-                                <span>X</span>
+                                <label>Usuario: </label>
+                                <span>{user.name}</span>
                             </div>
                             <div>
                                 <label>ID: </label>
-                                <span>X</span>
+                                <span>{user.id}</span>
                                 <span> - </span>
                                 <label>Username: </label>
-                                <span>X</span>
+                                <span>{user.username}</span>
                             </div>
                         </div>
-
-                        <div className="user-button">
-                            <input className="button-pay" type="button" value="Pagar" />
-                        </div>
-                        {user.name}
                     </div>
-
+                    <div className="user-button">
+                        <input className="button-pay" type="button" value="Pagar" />
+                    </div>
                 </div>
+
             </div>
+
         ))
     }
 };
