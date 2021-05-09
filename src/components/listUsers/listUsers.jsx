1ec184ae73ae85d51187c2b6249e7f1872cdab12
David Wilson de Jesus <davidwj15@gmail.com>
2021-05-09 14:49:43 -0300

    adicionado cartão e mensagens de sucesso ou erro

--- src/components/listUsers/listUsers.jsx
+++ src/components/listUsers/listUsers.jsx
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Axios from 'axios';
 import './../../components/listUsers/listUsers.css';
 import './../../components/listUsers/modal.css'
+
+
 
 export default class ListUsers extends Component {
     // estado do Array dos usuarios e variavel show
@@ -24,10 +26,16 @@ export default class ListUsers extends Component {
                     expiry_date: '01/20',
                 },
             ],
-            payment:false
+            value: [],
+            card: [],
+            success: [],
+            showResult: false
         };
         this.showModal = this.showModal.bind(this);
         this.hideModal = this.hideModal.bind(this);
+        this.showModalResult = this.showModalResult.bind(this);
+        this.hideModalResult = this.hideModalResult.bind(this);
+
     }
 
     showModal = () => {
@@ -38,6 +46,14 @@ export default class ListUsers extends Component {
         this.setState({ show: false });
     };
 
+    showModalResult = () => {
+        this.setState({ showResult: true });
+    };
+
+    hideModalResult = () => {
+        this.setState({ showResult: false });
+    };
+
     //pega as informações do usuario com axios - api
     componentDidMount() {
         Axios.get("https://www.mocky.io/v2/5d531c4f2e0000620081ddce")
@@ -46,80 +62,94 @@ export default class ListUsers extends Component {
                 this.setState({ users });
                 console.log(this.setState({ users }));
             });
-        
-
     };
+
+
     paymentUser = () => {
-        const payment = { card_number :document.getElementById('user_card_number'), 
-        value : document.getElementById('user-value')};
-        Axios.post("https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989", 
-        payment).then(response => {const payment = response.data;
-             this.setState({payment: true})});
-             console.log(this.setState({payment}));
-        };
+        const payments = {
+            card: document.getElementById('user-card-number'),
+            value: document.getElementById('user-value')
+        }
+        Axios.post("https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989",
+            payments).then(response => {
+                const success = response.data;
+                console.log(success);
+                this.setState({ success })
+                console.log(success);
+            });
+        this.showModalResult();
+        this.hideModal();
+    }
 
     // rendeniza as informações
-   
-    render() { 
+
+    render() {
         const { users, cards } = this.state;
         const showHideClassName = this.state.show ? "modal modal-show" : "modal modal-hide";
-        // const paymantFinish = 'Concluido com sucesso';
-        // const paymantError = 'Nao com sucesso';
+        const showHidePaySuccess = this.state.showResult ? "modal modal-show-success" : "modal modal-hide-success";
 
-        // if(this.state.payment.card_number = '1111111111111111'){
-        
-        // }
         return (<div>
-            {/* {if(this.state.payment.card_number = '1111111111111111'){
-                
-            }} */}
-            {/* modal */}
-            <div className={showHideClassName}  >
-                <div className="modal-title" >
-                    Pagamento para 
-                </div>
-                <div className="modal-body">
-                    <input className='user-value-pay modal-item' id='user-value'  type='number' placeholder='R$ 0,00' />
-                    <select name="cards" className='user-cards modal-item ' >
-                        {cards.map(card => (
-                            <option value={card.card_number} id='user_card_number'>Cartão com final {card.card_number.slice(-4)}</option>
-                        ))}
-                    </select>
-                    <div className='modal-button'>
-                        <input className="button-pay modal-item" type="button" value="Pagar" onClick={this.paymentUser} />
-                        <input className="button-pay modal-item" type="button" value="Cancela" onClick={this.hideModal} />
-                    </div>
-                </div>
-            </div>
-
             {/* rendeniza os usuarios */}
             {users.map(user => (
-                <div className="body-background" key={user.id}>
-                    <div className="user">
-                        <div className="user-itens">
-                            <div className="user-photo">
-                                <img src={user.img} alt={user.name} />
-                            </div>
-                            <div className="user-data">
-                                <div>
-                                    <label>Usuario: </label>
-                                    <span>{user.name}</span>
-                                </div>
-                                <div>
-                                    <label>ID: </label>
-                                    <span>{user.id}</span>
-                                    <span> - </span>
-                                    <label>Username: </label>
-                                    <span>{user.username}</span>
-                                </div>
+                <div> {/* modal */}
+                    <div className={showHideClassName}  >
+                        <div className="modal-title" >
+                            <div>Pagamento para <span className="modal-user-name">{user.name}</span></div>
+                            <input className="button-cancel modal-item" type="button" value="X" onClick={this.hideModal} />
+                        </div>
+                        <div className="modal-body">
+                            <input className='user-value-pay modal-item' id='user-value' type='number' placeholder='R$ 0,00' />
+                            <select name="cards" className='user-cards modal-item ' >
+                                {cards.map(card => (
+                                    <option value={card.card_number} id='user-card-number'>Cartão com final {card.card_number.slice(-4)}</option>
+                                ))}
+                            </select>
+                            <div className='modal-button'>
+                                <input className="button-pay modal-item" type="button" value="Pagar" onClick={this.paymentUser} />
                             </div>
                         </div>
-                        <div className="user-button">
-                            <input className="button-pay" type="button" value="Pagar" onClick={this.showModal} />
+                    </div>
+
+                     <div className={showHidePaySuccess}>
+                        <div className="modal-title">
+                            <div>Recibo de pagamento</div>
+                            <input className="button-cancel modal-item" type="button" value="X" onClick={this.hideModalResult} />
+
+                        </div>
+                        <div className="modal-body">
+                            {this.state.success ? "Pagamento foi concluido com sucesso." : "Pagamento não foi concluido com sucesso."}
+                        </div>
+                    </div> 
+
+
+                    <div className="body-background" key={user.id}>
+                        <div className="user">
+                            <div className="user-itens">
+                                <div className="user-photo">
+                                    <img src={user.img} alt={user.name} />
+                                </div>
+                                <div className="user-data">
+                                    <div>
+                                        <label>Usuario: </label>
+                                        <span>{user.name}</span>
+                                    </div>
+                                    <div>
+                                        <label>ID: </label>
+                                        <span>{user.id}</span>
+                                        <span> - </span>
+                                        <label>Username: </label>
+                                        <span>{user.username}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="user-button">
+                                <input className="button-pay" type="button" value="Pagar" onClick={this.showModal} />
+                            </div>
                         </div>
                     </div>
                 </div>
             ))}
+
 
         </div>);
     };
